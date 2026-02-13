af2a8fefa0c22c7f6af26d3a2ebe49623ced6def
Marco Molè <57618578+marcomole00@users.noreply.github.com>
2026-02-13 17:44:52 +0100

    Update silly.sh to install necessary packages
    Removed echo command and added package installations.

--- silly.sh
+++ silly.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "This is a silly script" > /tmp/silly.txt
-
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y build-essential git m4 scons zlib1g zlib1g-dev \
+    libprotobuf-dev protobuf-compiler libprotoc-dev libgoogle-perftools-dev \
+    python3-dev libboost-all-dev pkg-config python3-tk clang-format-15 
